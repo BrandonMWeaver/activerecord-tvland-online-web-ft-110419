5c5d7ad2617ef056f75810162137c4742cc481cc
Brandon Weaver <a7xncob@gmail.com>
2019-12-16 16:37:12 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,10 @@ class Show < ActiveRecord::Base
   
   has_many :actors, through: :characters
   
+  def actors_list
+    return actors.map do |actor|
+      actor.full_name
+    end
+  end
+  
 end
