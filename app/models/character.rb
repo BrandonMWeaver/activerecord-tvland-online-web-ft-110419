4b9b40c86988b6cb27abc306b46e2739fc8adfe3
Brandon Weaver <a7xncob@gmail.com>
2019-12-16 16:41:07 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,11 +1,9 @@
-require "pry"
 class Character < ActiveRecord::Base
   
   has_one :actor
   has_one :show
   
   def say_that_thing_you_say
-    binding.pry
     return self.catchphrase
   end
   
