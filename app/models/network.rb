984441228406f598eb481490c7f18b75e03d854d
Brandon Weaver <a7xncob@gmail.com>
2019-12-16 16:54:43 +0000

    Automatically backed up by Learn

--- app/models/network.rb
+++ app/models/network.rb
@@ -3,7 +3,7 @@ class Network < ActiveRecord::Base
   has_many :shows
 
   def sorry
-    "We're sorry about passing on John Mulaney's pilot"
+    return "We're sorry about passing on John Mulaney's pilot"
   end
   
 end
