3a6abc7e71377b4a198579ddc75709f3fe0ed9dd
Brandon Weaver <a7xncob@gmail.com>
2019-12-16 16:52:18 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,7 +4,7 @@ class Character < ActiveRecord::Base
   has_one :show
   
   def say_that_thing_you_say
-    return self.catchphrase
+    return "#{self.name} always says: #{self.catchphrase}"
   end
   
 end
